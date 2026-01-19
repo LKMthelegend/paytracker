3ebc0ec2122072aae31408c59602650cc38f8ad6
jetleelai <thelegendlkm@gmail.com>
2026-01-19 15:05:53 +0300

    feat: update settings page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.746vkb3c0mg"
+    "revision": "0.ar5igb38v1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
